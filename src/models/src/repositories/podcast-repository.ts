16553e0560bdbba8dd76d2d3b0e62b3883b023fc
rayllaalmeida46-lab <rayllaalmeida46@gmail.com>
2026-06-10 14:25:59 -0300

    Add podcast repository with sample podcasts

---
+++ src/models/src/repositories/podcast-repository.ts
@@ -0,0 +1,16 @@
+import { Podcast } from "../models/podcast-model";
+
+export const podcasts: Podcast[] = [
+  {
+    id: 1,
+    title: "Flow Podcast",
+    category: "Tecnologia",
+    episode: "NodeJS"
+  },
+  {
+    id: 2,
+    title: "Hipsters Ponto Tech",
+    category: "Programação",
+    episode: "TypeScript"
+  }
+];
